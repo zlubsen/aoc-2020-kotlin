5fa823ef5ca5f9c7789be32bae31a6cefa071f0e
Zeeger <zeeger@lubsen.eu>
2020-12-28 11:12:32 +0100

    Day 13 pt 2

--- src/main/kotlin/aoc2020/days/Day13pt2.kt
+++ src/main/kotlin/aoc2020/days/Day13pt2.kt
@@ -2,39 +2,40 @@ package aoc2020.days
 
 import aoc2020.utils.DayExercise
 import java.io.File
-import kotlin.math.abs
 
 class Day13pt2 : DayExercise {
     override fun run() {
         val lines = File("src/main/resources/aoc2020/days/day13.txt")
             .readLines()
 
-//        val lines = testInputDay13.lines()
-
         val entries = lines[1].split(",")
+
         val ids = findIdsAndIndices(entries)
         println(ids)
+
+        // First attempt - brute force
 //        println(findEarliestToMatchList(ids, 100000000000000L)) //100000000000000L
+
+        // Second attempt - efficient
+        println(findEarliestEfficient(ids))
     }
 }
 
-fun findIdsAndIndices(input : List<String>) : List<Pair<Int, Int>> {
-    val ids = mutableListOf<Pair<Int,Int>>()
-    for (idx in input.indices) {
-        if (input[idx] != "x")
-            ids.add(Pair(input[idx].toInt(), idx))
-    }
-    return ids
+fun findIdsAndIndices(input : List<String>) : List<Pair<Long, Long>> {
+    return input.map { if (it == "x") null else it.toLong() }
+        .mapIndexed { index, id -> if (id == null) null else id to index.toLong() }
+        .filterNotNull()
 }
 
-fun findEarliestToMatchList(ids : List<Pair<Int, Int>>, initial_t : Long) : Long {
+// START OF First attempt - brute force
+fun findEarliestToMatchList(ids : List<Pair<Long, Long>>, initial_t : Long) : Long {
     var found = false
     var t = initial_t
     val increment = ids.first().first
     while(!found) {
         var fail = false
         for (bus in ids) {
-            if ((t + bus.second.toLong()) % bus.first.toLong() != 0L) {
+            if ((t + bus.second) % bus.first != 0L) {
                 fail = true
                 break;
             }
@@ -47,17 +48,21 @@ fun findEarliestToMatchList(ids : List<Pair<Int, Int>>, initial_t : Long) : Long
     return t
 }
 
-fun solveChineseRemainderTheorem(ids : List<Pair<Int, Int>>) : Long {
-    val b_offsets = ids.map { it.second }
-    val bigN = ids.map { it.first }.reduce{ acc, it -> acc * it }
-//    val n_coefficients = ids.map { bigN / it.first }
-    val x_coefficients = ids.map { solveXi(bigN) }af
+// END OF First attempt - brute force
 
-    return 0L
-}
+// See: https://github.com/jmerle/advent-of-code-2020/blob/master/src/main/kotlin/com/jaspervanmerle/aoc2020/day/Day13.kt
+fun findEarliestEfficient(ids: List<Pair<Long, Long>>): Any {
+    return ids.fold(0L to 0L) { (currentOffset, multiplier), (busId, busOffset) ->
+        if (multiplier == 0L) {
+            return@fold 0L to busId
+        }
 
-fun solveXi(a : Int, mod : Int) : Int {
-    // solves xi for 'a'*xi ~ 1 (mod 'mod')
-    val remainder = a % mod
-    val xi = 1 /
+        var newOffset = currentOffset
+        while ((newOffset + busOffset) % busId != 0L) {
+            newOffset += multiplier
+        }
+        val acc = newOffset to multiplier * busId
+//        println("(${currentOffset}, ${multiplier}), (${busId}, ${busOffset}) -> (${acc.first}, ${acc.second})")
+        acc
+    }.first
 }
--- src/test/kotlin/aoc2020/days/Day13pt2KtTest.kt
+++ src/test/kotlin/aoc2020/days/Day13pt2KtTest.kt
@@ -6,26 +6,36 @@ import org.junit.jupiter.api.Test
 internal class Day13pt2KtTest {
     @Test
     fun day13pt2examples1() {
-        assertEquals(3417, findEarliestToMatchList(findIdsAndIndices(listOf("17", "x", "13", "19")), 0))
+        assertEquals(3417L, findEarliestToMatchList(findIdsAndIndices(listOf("17", "x", "13", "19")), 0))
+
+        assertEquals(3417L, findEarliestEfficient(findIdsAndIndices(listOf("17", "x", "13", "19"))))
     }
 
     @Test
     fun day13pt2examples2() {
-        assertEquals(754018, findEarliestToMatchList(findIdsAndIndices(listOf("67","7","59","61")), 0))
+        assertEquals(754018L, findEarliestToMatchList(findIdsAndIndices(listOf("67","7","59","61")), 0))
+
+        assertEquals(754018L, findEarliestEfficient(findIdsAndIndices(listOf("67","7","59","61"))))
     }
 
     @Test
     fun day13pt2examples3() {
-        assertEquals(779210, findEarliestToMatchList(findIdsAndIndices(listOf("67","x","7","59","61")), 0))
+        assertEquals(779210L, findEarliestToMatchList(findIdsAndIndices(listOf("67","x","7","59","61")), 0))
+
+        assertEquals(779210L, findEarliestEfficient(findIdsAndIndices(listOf("67","x","7","59","61"))))
     }
 
     @Test
     fun day13pt2examples4() {
-        assertEquals(1261476, findEarliestToMatchList(findIdsAndIndices(listOf("67","7","x","59","61")), 0))
+        assertEquals(1261476L, findEarliestToMatchList(findIdsAndIndices(listOf("67","7","x","59","61")), 0))
+
+        assertEquals(1261476L, findEarliestEfficient(findIdsAndIndices(listOf("67","7","x","59","61"))))
     }
 
     @Test
     fun day13pt2examples5() {
-        assertEquals(1202161486, findEarliestToMatchList(findIdsAndIndices(listOf("1789","37","47","1889")), 0))
+        assertEquals(1202161486L, findEarliestToMatchList(findIdsAndIndices(listOf("1789","37","47","1889")), 0))
+
+        assertEquals(1202161486L, findEarliestEfficient(findIdsAndIndices(listOf("1789","37","47","1889"))))
     }
 }
